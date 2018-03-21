d29e7efcc67a51bb19959e9e4ac9782059aaaf2e
LotuxPunk <clement@vandendaelen.com>
2018-03-21 08:53:01 +0100

    Principale complete

--- IG1_D_VandendaelenClement/Principale.c
+++ IG1_D_VandendaelenClement/Principale.c
@@ -4,9 +4,22 @@ void main(void) {
 	CodeErreur erreur = PAS_D_ERREUR;
 	NumMessage numMenu = MENU_PRINCIPAL;
 	Message *pLexique = NULL;
-	
-
 	erreur = chargerLexique(&pLexique);
+	if (erreur != PAS_D_ERREUR) {
+		if (erreur == ALLOCATION_MEMOIRE) {
+			printf("Erreur mémoire / Memory error");
+			libererLexique(pLexique);
+		}
+		else {
+			if (erreur == OUVERTURE_FICHIER) {
+				printf("Erreur ouverture fichier / File opening error");
+			}
+		}
+	}
+	else {
+		dialogue(pLexique);
+		libererLexique(pLexique);
+	}
 
 	
 
